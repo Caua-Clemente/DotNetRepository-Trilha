adb32f4beb5d3ae646c2e475c71278a016486be5
Caua-Clemente <cauaclementelima@gmail.com>
2024-02-23 15:15:34 -0300

    Criação de controllers, views e migrations

---
+++ ExemplosAula/Modulo5/MvcMovie/Migrations/20240223175818_StudioCreate.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MvcMovie.Migrations
+{
+    /// <inheritdoc />
+    public partial class StudioCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "StudioId",
+                table: "Movie",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Artist",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    Bio = table.Column<string>(type: "TEXT", nullable: true),
+                    Site = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Artist", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Studio",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    Country = table.Column<string>(type: "TEXT", nullable: true),
+                    Site = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Studio", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ArtistMovie",
+                columns: table => new
+                {
+                    ArtistsId = table.Column<int>(type: "INTEGER", nullable: false),
+                    MoviesId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ArtistMovie", x => new { x.ArtistsId, x.MoviesId });
+                    table.ForeignKey(
+                        name: "FK_ArtistMovie_Artist_ArtistsId",
+                        column: x => x.ArtistsId,
+                        principalTable: "Artist",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ArtistMovie_Movie_MoviesId",
+                        column: x => x.MoviesId,
+                        principalTable: "Movie",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Movie_StudioId",
+                table: "Movie",
+                column: "StudioId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ArtistMovie_MoviesId",
+                table: "ArtistMovie",
+                column: "MoviesId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Movie_Studio_StudioId",
+                table: "Movie",
+                column: "StudioId",
+                principalTable: "Studio",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Movie_Studio_StudioId",
+                table: "Movie");
+
+            migrationBuilder.DropTable(
+                name: "ArtistMovie");
+
+            migrationBuilder.DropTable(
+                name: "Studio");
+
+            migrationBuilder.DropTable(
+                name: "Artist");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Movie_StudioId",
+                table: "Movie");
+
+            migrationBuilder.DropColumn(
+                name: "StudioId",
+                table: "Movie");
+        }
+    }
+}
